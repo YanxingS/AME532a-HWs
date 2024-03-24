21dde0eac75cfb9d6cc6a3a5954c40f11cb738e7
YanxingS <yanxings@usc.edu>
2024-03-24 00:05:53 -0700

    problem 2 also done

--- HW7 problem 2/nonlinear_sim.m
+++ HW7 problem 2/nonlinear_sim.m
@@ -1,4 +1,4 @@
-clear all;
+clear all; close all; clc;
 
 %% run instruction
 % 1 enter name of the state equation that is in the m file that is in the
@@ -16,38 +16,51 @@ stat = fclose('all');
 runtime = input('enter run time:');
 dt = input('enter integration time-step:');
 N  = runtime/dt; k = 0; NP = fix(max(1,N/500)); time = 0;
-global xd x_new
-
-% calculates current state derivative
 xd = feval(name,time,x,u);
-
+global y
 for i = 0:N
     time = i*dt;
-
-    if time == 0 
-        % calculates new state 
-        [x_new] = x + dt*xd;
-    else 
-        % save old state derivative
-        old_xd = xd;
-        %calculates new state derivative
-        xd = feval(name,time,x_new,u);
-        % calculates new state
-        [x_new] = x_new + (0.5*dt)*(3*xd-old_xd);
-    end
-
     if rem(i,NP) == 0
         k = k+1;
-        y(k,1) = x_new(1);
-        y(k,2) = x_new(2);
+        y(k,1) = x(1);
+        y(k,2) = x(2);
+        y(k,3) = x(3);
     end
+    [x] = RK4(name,time,dt,x,u);
 end
 
 t= NP*dt*[0:k-1];
+
 figure(1)
-plot(y(:,1), y(:,2)) % For Van der Pol
-grid on
-axis([-3,3,-4,5])
+plot(y(:,1),y(:,2))
 xlabel('X(1)')
 ylabel('X(2)')
-text(-1.8,3.2,'(-2,3)')
+title('phase portrait')
+
+figure(2)
+timestep = 0:time/dt;
+plot(timestep,y(:,1))
+xlabel('time step')
+ylabel('X(10')
+title('wave form')
+
+
+
+%% compute solver (RK4 method)
+function [xnew]= RK4(f,time,dt,xx,u)
+xd=feval(f,time,xx,u);
+xa=xd*dt;
+x =xx + 0.5*xa;
+t =time + 0.5*dt;
+xd=feval(f,t,x,u);
+q = xd*dt;
+x = xx + 0.5*q;
+xa= xa + 2.0*q;
+xd= feval(f,t,x,u);
+q = xd*dt;
+x = xx + q;
+xa= xa + 2.0*q;
+time= time + dt;
+xd= feval(f,time,x,u);
+xnew= xx + (xa + xd*dt)/6.0;
+end
---
+++ HW7 problem 2/vdp.m
@@ -0,0 +1,5 @@
+% create initial condition file 
+
+ic = [3 1 0.1 0.1 0.1 0.8];
+
+save 'vdp.dat' ic -ascii
